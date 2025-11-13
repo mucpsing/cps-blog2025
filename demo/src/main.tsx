6acaefdc30ae70afcfa0cfc5ea67f41860ac7a3c
Capsion <373704015@qq.com>
2025-11-13 23:20:30 +0800

    feat: 添加粒子效果demo

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: cpasion-office-win10 373704015@qq.com
  * @Date: 2025-03-05 09:29:31
- * @LastEditors: cpasion-office-win10 373704015@qq.com
- * @LastEditTime: 2025-11-12 10:24:02
+ * @LastEditors: Capsion 373704015@qq.com
+ * @LastEditTime: 2025-11-12 22:43:44
  * @FilePath: \gsap-lenis-learn\src\main.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
